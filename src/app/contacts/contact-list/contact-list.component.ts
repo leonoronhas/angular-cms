d0483c0ab56d65be414bb76fb29931600f2f756f
Leonardo <leosud099@gmail.com>
2021-01-23 23:10:00 -0700

    Fixed styling

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Contact } from '../../contacts/contact.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Contact } from '../../contacts/contact.model';
   styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent implements OnInit {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [
     {
       id: '1',
@@ -29,4 +30,7 @@ export class ContactListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
 }
