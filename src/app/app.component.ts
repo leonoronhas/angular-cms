b05a891d3d3d682ac63a3629977893fad05b47b1
Leonardo <leosud099@gmail.com>
2021-01-23 23:08:16 -0700

    Added new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cms-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-cms';
+  selectedFeature = 'documents';
+
+  switchView(selectedFeature: string) {
+    this.selectedFeature = selectedFeature;
+  }
 }
