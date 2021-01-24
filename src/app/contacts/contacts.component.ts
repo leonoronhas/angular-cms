4188cdd592e99d21d950b90e1790edf3bf45f909
Leonardo <leosud099@gmail.com>
2021-01-23 23:08:59 -0700

    Added details for contact items

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contacts/contact.model';
 @Component({
   selector: 'cms-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css'],
 })
 export class ContactsComponent implements OnInit {
-
+  selectedContact: Contact;
   constructor() {}
 
   ngOnInit(): void {}
