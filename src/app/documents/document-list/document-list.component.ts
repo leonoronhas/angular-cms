17d7b03f4593d38747b0f6255688eddaa0d498de
Leonardo <leosud099@gmail.com>
2021-01-30 20:00:47 -0700

    Added logic for list of docs

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedCDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('1', 'Leo', 'Team 1', 'https://www.google.com', null),
+    new Document('2', 'Thyne', 'Team 2', 'https://www.google.com', null),
+    new Document('3', 'Giovanva', 'Team 3', 'https://www.google.com', null),
+    new Document('4', 'Sophia', 'Team 4', 'https://www.google.com', null),
+    new Document('5', 'Francisco', 'Team 5', 'https://www.google.com', null),
+    new Document('6', 'Stuart', 'Little', 'https://www.google.com', null),
+    new Document('7', 'Jacob', 'Doc 2', 'https://www.google.com', null),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedCDocumentEvent.emit(document);
   }
-
 }
